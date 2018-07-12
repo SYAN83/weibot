5c03f16f42e8f63eab94cfda251ededb8096a7f6
Shu Yan <yanshu.usc@gmail.com>
2018-07-12 03:49:41 +0000

    modify sche

--- schedule.py
+++ schedule.py
@@ -9,26 +9,26 @@ logging.basicConfig()
 logging.getLogger('apscheduler').setLevel(logging.DEBUG)
 
 
-def main(duration: int=12, interval: int=1, unit: str='hours'):
-
-    duration = duration
-    if unit == 'hours':
-        duration *= 3600
-    elif unit == 'minutes':
-        duration *= 60
-    else:
-        raise ValueError('Invalid time unit')
-
+def main(duration: int=24, interval: int=1, time_unit: str='hours'):
+    sleep_time = duration
+    if duration > 0:
+        if time_unit == 'hours':
+            sleep_time *= 3600
+        elif time_unit == 'minutes':
+            sleep_time *= 60
+        else:
+            raise ValueError('Invalid time unit')
     with open('./credentials.yml') as f:
         cred = yaml.load(f)
     weibot = Weibot(mongo_credentials=cred['mongo'], weibo_credentials=cred['weibo'])
     scheduler = BackgroundScheduler()
-    job = scheduler.add_job(weibot.crawl, 'interval', **{unit:interval})
+    job = scheduler.add_job(weibot.crawl, 'interval', **{time_unit:interval})
     logging.info(job)
     scheduler.start()
-    time.sleep(duration)
-    scheduler.shutdown(wait=True)
+    if duration > 0:
+        time.sleep(duration)
+        scheduler.shutdown(wait=True)
 
 
 if __name__ == '__main__':
-    main()
+    main(duration=48)
